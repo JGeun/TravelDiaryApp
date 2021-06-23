e2757b194753564d14e3938ac19e570becd14d69
JGeun <pjk8926@gmail.com>
2021-06-24 00:32:21 +0900

    main

--- app/src/main/java/com/hansung/traveldiary/MainActivity.kt
+++ app/src/main/java/com/hansung/traveldiary/MainActivity.kt
@@ -7,11 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
 
         println("준호")
-=======
-        println("ㅇㄹㅇㄴㅁㄹ")
->>>>>>> yjh
     }
 }
