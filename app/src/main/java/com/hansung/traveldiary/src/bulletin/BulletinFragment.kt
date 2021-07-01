c116d6f78ed0a7f2fc9329ccf5712e6acf9a26a7
yjh-1008 <exesea12@gmail.com>
2021-07-01 14:10:40 +0900

    플로팅 버튼 색상 디자인 추가조정 메뉴추가

--- app/src/main/java/com/hansung/traveldiary/src/bulletin/BulletinFragment.kt
+++ app/src/main/java/com/hansung/traveldiary/src/bulletin/BulletinFragment.kt
@@ -1,18 +1,16 @@
 package com.hansung.traveldiary.src.bulletin
 
-import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hansung.traveldiary.R
-import com.hansung.traveldiary.databinding.ActivityMainBinding.inflate
 import com.hansung.traveldiary.databinding.FragmentBulletinBinding
-import com.hansung.traveldiary.databinding.FragmentHomeBinding
 
 class BulletinFragment : Fragment(){
+
     private lateinit var binding : FragmentBulletinBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,8 +35,22 @@ class BulletinFragment : Fragment(){
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bulletn,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.searchIcon-> println("돋보기")
+            R.id.overflow->println("더보기")
+        }
+        return true
+    }
     override fun onContextItemSelected(item: MenuItem): Boolean {
 
         return super.onContextItemSelected(item)
     }
+
+
 }
--- app/src/main/java/com/hansung/traveldiary/src/bulletin/PostActivity.kt
+++ app/src/main/java/com/hansung/traveldiary/src/bulletin/PostActivity.kt
@@ -2,6 +2,7 @@ package com.hansung.traveldiary.src.bulletin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hansung.traveldiary.R
 import com.hansung.traveldiary.databinding.ActivityPostBinding
@@ -10,7 +11,7 @@ class PostActivity : AppCompatActivity() {
     private val binding by lazy{
         ActivityPostBinding.inflate(layoutInflater)
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.rv2.setHasFixedSize(true)
@@ -19,6 +20,8 @@ class PostActivity : AppCompatActivity() {
                 DiaryData(R.drawable.gwangwhamun),
                 DiaryData(R.drawable.gwangwhamun)
         )
+        val adapter= DiaryAdapter(PostryList)
+        binding.rv2.adapter=adapter
         binding.rv2.layoutManager=LinearLayoutManager(this)
     }
 }
