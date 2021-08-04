71a1b749e0b97a883af1dba209461f21d65072b8
jungyeeun00 <ekfqlc0103@naver.com>
2021-08-04 23:49:12 +0900

    edit diary image activity

---
+++ app/src/main/java/com/hansung/traveldiary/src/diary/write_diary/EditImageAdapter.kt
@@ -0,0 +1,27 @@
+package com.hansung.traveldiary.src.diary.write_diary
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.hansung.traveldiary.databinding.ItemEditDiaryImageBinding
+
+class EditImageAdapter(val imageArrayList: ArrayList<Drawable>) : RecyclerView.Adapter<EditImageAdapter.ViewHolder>() {
+    class ViewHolder(val binding : ItemEditDiaryImageBinding) : RecyclerView.ViewHolder(binding.root) {
+        var image : ImageView = binding.itemEditDiaryImage
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemEditDiaryImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.itemView.context
+        Glide.with(context).load(imageArrayList[position]).into(holder.image)
+    }
+
+    override fun getItemCount(): Int = imageArrayList.size
+}
