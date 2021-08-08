9e5259ddffd8b79603b0c11d73238c01cd8f544c
JGeun <pjk8926@gmail.com>
2021-08-08 22:30:19 +0900

    control images in WriteDayFragment

--- app/src/main/java/com/hansung/traveldiary/src/diary/write_diary/WriteDayDiaryFragment.kt
+++ app/src/main/java/com/hansung/traveldiary/src/diary/write_diary/WriteDayDiaryFragment.kt
@@ -1,11 +1,15 @@
 package com.hansung.traveldiary.src.diary.write_diary
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -21,48 +25,58 @@ import com.hansung.traveldiary.src.plan.TravelPlanMapFragment
 import com.hansung.traveldiary.src.plan.model.SharedPlaceViewModel
 
 class WriteDayDiaryFragment(val index: Int, val day: Int) : Fragment() {
-    private lateinit var btmSheetFragment : SelectDayBtmSheetFragment
-    private val viewModel : SelectDayViewModel by activityViewModels()
+    private val binding: FragmentMakeDiaryDaySectionBinding by lazy {
+        FragmentMakeDiaryDaySectionBinding.inflate(layoutInflater)
+    }
+    private lateinit var btmSheetFragment: SelectDayBtmSheetFragment
+    private val viewModel: SelectDayViewModel by activityViewModels()
     private var user: FirebaseUser? = null
     private var db: FirebaseFirestore? = null
-    private val userPlanDataModel : SharedPlaceViewModel by viewModels()
-    private lateinit var transaction : FragmentTransaction
-    private lateinit var travelPlanMapFragment : TravelPlanMapFragment
+    private val userPlanDataModel: SharedPlaceViewModel by viewModels()
+    private lateinit var transaction: FragmentTransaction
+    private lateinit var travelPlanMapFragment: TravelPlanMapFragment
+
+    private val diaryInfo = MainActivity.userDiaryArray[index].diaryArray[day].diaryInfo
+
+    private lateinit var updateImageTask: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentMakeDiaryDaySectionBinding.inflate(layoutInflater)
-
-        Log.d("과정", "WriteAdapter-ViewModel Data: ${viewModel.dayData.value!!}")
-        println("fragment 시작")
-
-        val diaryInfo = MainActivity.userDiaryArray[index].diaryArray[day].diaryInfo
-        if(diaryInfo.imagePathArray.size == 0){
-            binding.uploadViewPager.isVisible = false
-            binding.indicator.isVisible = false
-        }else{
-            binding.uploadViewPager.isVisible = true
-            binding.indicator.isVisible = true
-            binding.uploadViewPager.adapter =
-                WriteImageAdapter(diaryInfo.imagePathArray)
-            binding.uploadViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-            //인디케이터
-            binding.indicator.setViewPager(binding.uploadViewPager)
-        }
 
         transaction = childFragmentManager.beginTransaction()
 
         binding.uploadContents.setText(diaryInfo.diaryContents)
 
-        btmSheetFragment= SelectDayBtmSheetFragment(MainActivity.userDiaryArray[index].diaryArray.size)
-        val dayText = "${viewModel.dayData.value!! +1}일차 일기"
+        btmSheetFragment =
+            SelectDayBtmSheetFragment(MainActivity.userDiaryArray[index].diaryArray.size)
+        val dayText = "${viewModel.dayData.value!! + 1}일차 일기"
         binding.atpTvDays.text = dayText
 
-        binding.daySelectLayout.setOnClickListener{
-            btmSheetFragment.show(childFragmentManager,btmSheetFragment.tag)
+        binding.daySelectLayout.setOnClickListener {
+            btmSheetFragment.show(childFragmentManager, btmSheetFragment.tag)
+        }
+
+        if (diaryInfo.imagePathArray.size == 0) {
+            binding.uploadViewPager.isVisible = false
+            binding.indicator.isVisible = false
+        } else {
+            binding.uploadViewPager.isVisible = true
+            binding.indicator.isVisible = true
+            binding.uploadViewPager.adapter = WriteImageAdapter(diaryInfo.imagePathArray)
+            binding.uploadViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            //인디케이터
+            binding.indicator.setViewPager(binding.uploadViewPager)
+        }
+
+        updateImageTask = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                println("WriteDayFragment RESULT_OK!!!!!!!!!!!!!")
+            }
         }
 
         binding.uploadDiaryAddbtn.setOnClickListener {
@@ -73,5 +87,25 @@ class WriteDayDiaryFragment(val index: Int, val day: Int) : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("WriteDayFragment ${day} Start!!!!!!!!!!!!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("WriteDayFragment ${day} PAUSE!!!!!!!!!!!!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("WriteDayFragment ${day} PAUSE!!!!!!!!!!!!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("WriteDayFragment ${day} DESTROY!!!!!!!!!!!!")
     }
 }
--- app/src/main/java/com/hansung/traveldiary/src/diary/write_diary/WriteDiaryActivity.kt
+++ app/src/main/java/com/hansung/traveldiary/src/diary/write_diary/WriteDiaryActivity.kt
@@ -3,6 +3,8 @@ package com.hansung.traveldiary.src.diary.write_diary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.hansung.traveldiary.R
 import com.hansung.traveldiary.databinding.ActivityWriteDiaryBinding
@@ -14,13 +16,13 @@ class WriteDiaryActivity : AppCompatActivity() {
         ActivityWriteDiaryBinding.inflate(layoutInflater)
     }
 
-    private val viewModel : SelectDayViewModel by viewModels()
+    private val viewModel: SelectDayViewModel by viewModels()
     private var index = 0
     private var day = 0
 
-    companion object{
-        val fragmentArray = ArrayList<WriteDayDiaryFragment>()
-    }
+    private val fragmentArray = ArrayList<WriteDayDiaryFragment>()
+
+    private lateinit var updateImageTask: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +32,11 @@ class WriteDiaryActivity : AppCompatActivity() {
         index = intent.getIntExtra("index", 0)
         day = intent.getIntExtra("day", 0)
 
-        viewModel.dayData.observe(this){
-            println("들어옴")
-            val transaction2 = supportFragmentManager.beginTransaction().replace(R.id.framelayout, fragmentArray[viewModel.dayData.value!!])
+        println("WriteDiaryActivity CREATE!!!!!")
+
+        viewModel.dayData.observe(this) {
+            val transaction2 = supportFragmentManager.beginTransaction()
+                .replace(R.id.framelayout, fragmentArray[viewModel.dayData.value!!])
             transaction2.commit()
         }
 
@@ -43,20 +47,21 @@ class WriteDiaryActivity : AppCompatActivity() {
         }
 
         binding.showPlacelist.setOnClickListener {
-            var intent = Intent(it.context, ShowPlacelistActivity::class.java)
+            val intent = Intent(it.context, ShowPlacelistActivity::class.java)
             intent.putExtra("index", index)
             intent.putExtra("day", day)
             startActivity(intent)
         }
-
     }
+
     override fun onStart() {
         super.onStart()
-        fragmentArray.clear()
 
+        fragmentArray.clear()
         for (i in 0 until MainActivity.userDiaryArray[index].diaryArray.size) {
             fragmentArray.add(WriteDayDiaryFragment(index, i))
         }
+        println("WriteDiaryActivity START!!!!! fragmentArray size: ${fragmentArray.size}")
 
         viewModel.setDay(day)
         val transaction = supportFragmentManager.beginTransaction().replace(R.id.framelayout, fragmentArray[viewModel.dayData.value!!])
