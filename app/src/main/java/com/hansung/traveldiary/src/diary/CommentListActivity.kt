60bce993ce5fe877a580d53a4240b1f6732c7f27
yjh-1008 <exesea12@gmail.com>
2021-08-19 23:06:22 +0900

    디자인 수정

--- app/src/main/java/com/hansung/traveldiary/src/diary/CommentListActivity.kt
+++ app/src/main/java/com/hansung/traveldiary/src/diary/CommentListActivity.kt
@@ -1,10 +1,14 @@
 package com.hansung.traveldiary.src.diary
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hansung.traveldiary.R
 import com.hansung.traveldiary.databinding.ActivityCommentListBinding
+import com.hansung.traveldiary.src.plan.KakaoSearchKeywordService
 
 class CommentListActivity : AppCompatActivity() {
     private val binding by lazy{
@@ -22,5 +26,17 @@ class CommentListActivity : AppCompatActivity() {
         binding.ivX.setOnClickListener {
             finish()
         }
+        window.statusBarColor = Color.parseColor("#2C2C2C")
+        binding.writeComment.setOnKeyListener(object : View.OnKeyListener {
+
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+
+                if ((event!!.action == KeyEvent.ACTION_DOWN) && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    println("클릭을 했따")
+                    return true
+                }
+                return false
+            }
+        })
     }
 }
--- app/src/main/java/com/hansung/traveldiary/src/diary/CommentsAdapter.kt
+++ app/src/main/java/com/hansung/traveldiary/src/diary/CommentsAdapter.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.hansung.traveldiary.R
 import com.hansung.traveldiary.databinding.ItemCommentsListBinding
-import com.hansung.traveldiary.databinding.ItemDiaryPictureBinding
 
 class CommentsAdapter():RecyclerView.Adapter<CommentsAdapter.PagerViewHolder>() {
     class PagerViewHolder(val binding:ItemCommentsListBinding)
@@ -16,7 +15,6 @@ class CommentsAdapter():RecyclerView.Adapter<CommentsAdapter.PagerViewHolder>() 
         val nickname=binding.tvNickname
         val comment=binding.tvCommet
         val date=binding.tvDate
-        val iconLike=binding.ivLike
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerViewHolder {
@@ -27,15 +25,7 @@ class CommentsAdapter():RecyclerView.Adapter<CommentsAdapter.PagerViewHolder>() 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
         Glide.with(holder.itemView.context).load(R.drawable.bg_profile).circleCrop().into(holder.profileImage)
         var chk_like=false
-        holder.iconLike.setOnClickListener {
-            if(!chk_like){
-                Glide.with(holder.itemView.context).load(R.drawable.ic_full_heart).into(holder.iconLike)
-                chk_like=true
-            }else{
-                Glide.with(holder.itemView.context).load(R.drawable.ic_heart).into(holder.iconLike)
-                chk_like=false
-            }
-        }
+
     }
 
     override fun getItemCount(): Int = 3
