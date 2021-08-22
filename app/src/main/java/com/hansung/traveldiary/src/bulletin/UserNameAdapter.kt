f75527e280a4a7502fc6229cafd7e97da10236b9
yjh-1008 <exesea12@gmail.com>
2021-08-22 21:39:29 +0900

    검색시 어댑터 변경

--- app/src/main/java/com/hansung/traveldiary/src/bulletin/UserNameAdapter.kt
+++ app/src/main/java/com/hansung/traveldiary/src/bulletin/UserNameAdapter.kt
@@ -2,29 +2,33 @@ package com.hansung.traveldiary.src.bulletin
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
+import android.opengl.Visibility
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.startActivity
+import android.widget.Filter
 import androidx.core.content.res.ResourcesCompat
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.hansung.traveldiary.R
 import com.hansung.traveldiary.databinding.ItemBulletinBinding
+import com.hansung.traveldiary.src.BulletinData
 import com.hansung.traveldiary.src.MainActivity
 import com.hansung.traveldiary.src.diary.CommentListActivity
 
-class BulletinAdapter():RecyclerView.Adapter<BulletinAdapter.ViewHolder>() {
+class UserNameAdapter(val items:ArrayList<BulletinData>) : RecyclerView.Adapter<UserNameAdapter.ViewHolder>() {
     private val db = Firebase.firestore
-    inner class ViewHolder(binding: ItemBulletinBinding):RecyclerView.ViewHolder(binding.root){
+    private val viewModel=SearchWordVIewModel()
+    inner class ViewHolder(binding: ItemBulletinBinding): RecyclerView.ViewHolder(binding.root){
+        val layout=binding.layout
         val userImage = binding.btItemUserImage
         val userName = binding.btItemUserName
         val likeCnt=binding.btItemTvLikecnt
@@ -34,50 +38,68 @@ class BulletinAdapter():RecyclerView.Adapter<BulletinAdapter.ViewHolder>() {
         var url=""
         //val thumbnail = binding.btItemIvThumbnail
     }
+    private var count=0
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder {
 
         val binding= ItemBulletinBinding.inflate(LayoutInflater.from(parent.context), parent,false)
-
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val context = holder.itemView.context
-        val data = MainActivity.bulletinDiaryArray[position].userDiaryData
+        val data = items[position].userDiaryData
         println("BUlletinAdapter에 들어옴")
-        holder.userName.text = MainActivity.bulletinDiaryArray[position].userInfo.nickname
-        val userImagePath = MainActivity.bulletinDiaryArray[position].userInfo.profileImage
-        if(userImagePath == "")
-            Glide.with(context).load(ResourcesCompat.getDrawable(context.resources, R.drawable.img_beach, null)).circleCrop().into(holder.userImage)
+        println(items.size)
+        //println(MainActivity.bulletinDiaryArray[position].userInfo.nickname)
+        holder.userName.text = items[position].userInfo.nickname
+
+
+        val userImagePath =items[position].userInfo.profileImage
+        if (userImagePath == "")
+            Glide.with(context).load(
+                ResourcesCompat.getDrawable(
+                    context.resources,
+                    R.drawable.img_beach,
+                    null
+                )
+            ).circleCrop().into(holder.userImage)
         else
-            Glide.with(context).load(userImagePath.toString()).circleCrop().into(holder.userImage)
+            Glide.with(context).load(userImagePath.toString()).circleCrop()
+                .into(holder.userImage)
 
         holder.viewpager.adapter = BulletinViewPagerAdapter(position)
         holder.viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         //Glide.with(context).load(data.diaryBaseData.mainImage).into(holder.thumbnail)
-        holder.likeCnt.text=data.baseData.like.toString()
-        holder.comment.text=data.baseData.comments.toString()
+        holder.likeCnt.text = data.baseData.like.toString()
+        holder.comment.text = data.baseData.comments.toString()
         holder.itemView.setTag(position)
-        holder.url=userImagePath.toString()
+        holder.url = userImagePath.toString()
 
-        holder.itemView.setOnClickListener{
-            val intent = Intent(context,BulletinDaySectionActivity::class.java)
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, BulletinDaySectionActivity::class.java)
             intent.putExtra("index", position)
             context.startActivity(intent)
         }
 
         holder.userImage.setOnClickListener {
-            val intent = Intent(context,OtherUserActivity::class.java)
+            val intent = Intent(context, OtherUserActivity::class.java)
             intent.putExtra("index", position)
             context.startActivity(intent)
         }
 
-        holder.comment.setOnClickListener { 
-            val intent= Intent(context,CommentListActivity::class.java)
+        holder.comment.setOnClickListener {
+            val intent = Intent(context, CommentListActivity::class.java)
             context.startActivity(intent)
         }
-
     }
 
-    override fun getItemCount(): Int = MainActivity.bulletinDiaryArray.size
+    override fun getItemCount(): Int = items.size
 }
+
+
+
+
+
+
+
+
--- app/src/main/java/com/hansung/traveldiary/src/bulletin/UserNameFragment.kt
+++ app/src/main/java/com/hansung/traveldiary/src/bulletin/UserNameFragment.kt
@@ -1,21 +1,19 @@
 package com.hansung.traveldiary.src.bulletin
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
-import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.hansung.traveldiary.R
 import com.hansung.traveldiary.databinding.FragmentUserNameBinding
+import com.hansung.traveldiary.src.BulletinData
+import com.hansung.traveldiary.src.MainActivity
 
 class UserNameFragment : Fragment() {
+    var items: ArrayList<BulletinData> =ArrayList<BulletinData>()
     private val viewModel:SearchWordVIewModel by activityViewModels()
     private lateinit var binding:FragmentUserNameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,16 +26,35 @@ class UserNameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         binding=FragmentUserNameBinding.inflate(inflater, container, false)
+        var searchWord:String
+        val searchArray= BulletinData()
+
+
         viewModel.searchWord.observe(viewLifecycleOwner){
+            items.clear()
             Log.d("췤","변경된 값은 ${viewModel.searchWord.value}")
+            searchWord=viewModel.searchWord.value.toString()
+            println("새롭게 넣은 리스트의 길이"+items.size)
+            binding.rv.apply {
+                setHasFixedSize(true)
+                for(i in 0..MainActivity.bulletinDiaryArray.size-1){
+                    println(MainActivity.bulletinDiaryArray[i].userInfo.nickname+"                   "+searchWord.toString())
+                    if(MainActivity.bulletinDiaryArray[i].userInfo.nickname.equals(searchWord.toString())){
+                        println(MainActivity.bulletinDiaryArray[i].userInfo.nickname)
+                        items.add(MainActivity.bulletinDiaryArray[i])
+                    }
+                }
+                println("새롭게 넣은 리스트의 길이       "+items.size)
+                println()
+                adapter = UserNameAdapter(items)
+                layoutManager = LinearLayoutManager(context)
+
+            }
+
         }
         //binding.rv.isVisible=false
-        binding.rv.apply {
-            setHasFixedSize(true)
-            adapter = BulletinAdapter()
-            layoutManager = LinearLayoutManager(context)
-        }
 
         return binding.root
     }
