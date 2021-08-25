ef383bc2451c203e7ce57c8f4da8d0e311db30dd
jungyeeun00 <ekfqlc0103@naver.com>
2021-08-26 04:48:29 +0900

    GimpoAirportTip Activity

--- app/src/main/java/com/hansung/traveldiary/src/diary/CommentsAdapter.kt
+++ app/src/main/java/com/hansung/traveldiary/src/diary/CommentsAdapter.kt
@@ -19,7 +19,7 @@ import com.hansung.traveldiary.src.CommentsData
 import com.hansung.traveldiary.src.CommentsFolder
 import com.hansung.traveldiary.src.MainActivity
 
-class CommentsAdapter(val commentsList: CommentsFolder):RecyclerView.Adapter<CommentsAdapter.PagerViewHolder>() {
+class CommentsAdapter(val commentsList: CommentsFolder, val index: Int):RecyclerView.Adapter<CommentsAdapter.PagerViewHolder>() {
     class PagerViewHolder(val binding:ItemCommentsListBinding)
         :RecyclerView.ViewHolder(binding.root){
         val profileImage=binding.ivProfile
@@ -95,19 +95,11 @@ class CommentsAdapter(val commentsList: CommentsFolder):RecyclerView.Adapter<Com
                             builder.show()
                         }
                         1 -> { //삭제
-                            var idx = 0L
-                            commentsList.commentsFolder.removeAt(position)
-//                        if(myDiary){
-//                            Log.d("체크", "MyDiary")
+//                            var idx = 0L
+//                            commentsList.commentsFolder.removeAt(position)
 //                            idx = MainActivity.userDiaryArray[index].baseData.idx
-////                            diaryUserEmail = MainActivity.userDiaryArray[index].baseData.userEmail
 //                            MainActivity.userDiaryArray[index].baseData.comments.commentsFolder.remove(data)
-//                        }else{
-//                            Log.d("체크", "Bulletin")
-//                            idx = MainActivity.bulletinDiaryArray[index].userDiaryData.baseData.idx
-////                            diaryUserEmail = MainActivity.bulletinDiaryArray[index].userDiaryData.baseData.userEmail
-//                            MainActivity.bulletinDiaryArray[index].userDiaryData.baseData.comments.commentsFolder.remove(data)
-//                        }
+                            Log.d("체크", "$index")
 
                             Log.d("유저 이메일", data.userEmail)
 //                        db.collection("Diary").document(data.userEmail)
