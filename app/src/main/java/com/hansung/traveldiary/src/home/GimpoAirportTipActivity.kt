ec5a44aae5d7f3344798e01975d1315d5b94ad93
JGeun <pjk8926@gmail.com>
2021-08-29 21:42:39 +0900

    check all

--- app/src/main/java/com/hansung/traveldiary/src/home/GimpoAirportTipActivity.kt
+++ app/src/main/java/com/hansung/traveldiary/src/home/GimpoAirportTipActivity.kt
@@ -3,6 +3,7 @@ package com.hansung.traveldiary.src.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hansung.traveldiary.databinding.ActivityGimpoAirportTipBinding
+import com.hansung.traveldiary.util.StatusBarUtil
 
 class GimpoAirportTipActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -13,6 +14,7 @@ class GimpoAirportTipActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        StatusBarUtil.setStatusBarColor(this, StatusBarUtil.StatusBarColorType.WHITE_STATUS_BAR)
         binding.ivBack.setOnClickListener {
             finish()
         }
--- app/src/main/java/com/hansung/traveldiary/src/home/HistoryPlaceFragment.kt
+++ app/src/main/java/com/hansung/traveldiary/src/home/HistoryPlaceFragment.kt
@@ -52,7 +52,6 @@ class HistoryPlaceFragment () : Fragment() {
             layoutManager = LinearLayoutManager(context)
         }
 
-
         return binding.root
     }
 }
